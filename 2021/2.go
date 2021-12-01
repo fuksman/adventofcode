60a114fbf2edf607cc78ce13f838ff07d93069a2
Sergey Fuksman <sergey@fuksman.com>
2021-12-01 09:51:44 +0300

    December 1, 2021

---
+++ 2021/2.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("1.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	measurements := []int64{}
+	for scanner.Scan() {
+		current, _ := strconv.ParseInt(scanner.Text(), 10, 0)
+		measurements = append(measurements, current)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	counter := 0
+	for i := 0; i < len(measurements)-3; i++ {
+		if measurements[i]+measurements[i+1]+measurements[i+2] < measurements[i+3]+measurements[i+1]+measurements[i+2] {
+			counter++
+		}
+	}
+
+	fmt.Println(counter)
+}
